69c85589e3c193059b4274417ea256673af5aabc
Minjoo Park <cathymjp@gmail.com>
2023-01-17 07:06:30 +0000

    files removed for cleanup

--- calculate_PSNR.m
+++
@@ -1,7 +0,0 @@
-
-function PSNR =calculate_PSNR(f,F)
-    GL = 255;  
-    MSE=mean2((double(f)-double(F)).^2);
-    PSNR= 10*log10( (GL*GL)/MSE );
-end   
-   
--- calculate_corr.m
+++
@@ -1,3 +0,0 @@
-function C2 =calculate_corr(original,filtered)
-   C2 = corr2(original(:),filtered(:));
-end
